7edea6913f1b072f17d7162ff7e9d1255a2a125c
hachihao792001 <hachihao792001@gmail.com>
2022-04-21 22:21:00 +0700

    page san pham done

---
+++ js/admin_sanpham.js
@@ -0,0 +1,61 @@
+const product_list = [
+    {
+        ID: 1,
+        image: "./assets/images/example_logo.jpeg",
+        name: "PhinDi Hạnh Nhân",
+        stock: 5,
+        price: 39000,
+        description: "Iced PHIN Coffee with Almond Milk",
+    },
+    {
+        ID: 2,
+        image: "./assets/images/example_logo.jpeg",
+        name: "PhinDi Hạnh Nhân",
+        stock: 5,
+        price: 39000,
+        description: "Iced PHIN Coffee with Almond Milk",
+    },
+    {
+        ID: 3,
+        image: "./assets/images/example_logo.jpeg",
+        name: "PhinDi Hạnh Nhân",
+        stock: 5,
+        price: 39000,
+        description: "Iced PHIN Coffee with Almond Milk",
+    },
+    {
+        ID: 4,
+        image: "./assets/images/example_logo.jpeg",
+        name: "PhinDi Hạnh Nhân",
+        stock: 5,
+        price: 39000,
+        description: "Iced PHIN Coffee with Almond Milk",
+    },
+];
+
+console.log("hello");
+
+var app = document.querySelector("#table_content");
+
+var items = product_list
+    .map(function (item) {
+        return `
+        <tr>
+            <td scope="row">${item.ID}</td>
+            <td>
+                <img src="${item.image}" width="40" height="40" class="rounded-circle" />
+            </td>
+            <td>${item.name}</td>
+            <td>${item.stock}</td>
+            <td>${item.price}</td>
+            <td>${item.description}</td>
+            <td>
+                <a href="#"><i class="fa-solid fa-pen fa-2xl"></i></a>
+            </td>
+        </tr>
+        `;
+    })
+    .join("");
+
+console.log(items);
+app.innerHTML = items;
