8f146e7531a59619678ff7a9923426ba8d505619
TranPhuongDinh <dinhblade123@gmail.com>
2022-06-14 11:04:25 +0700

    add slug and categoryId for Product, added Category model

--- src/app/models/Category.js
+++ src/app/models/Category.js
@@ -7,19 +7,13 @@ mongoose.plugin(slugUpdater);
 
 const Schema = mongoose.Schema;
 
-const Product = new Schema(
+const Category = new Schema(
     {
         _id: { type: Number },
-        image: { type: String },
-        cloudinaryId: { type: String },
         name: { type: String },
-        number: { type: Number },
-        price: { type: Number },
-        description: { type: String },
-        categoryId: { type: Number },
         slug: { type: String, slug: "name", unique: true },
     },
     { timestamps: true, _id: false }
 );
 
-module.exports = mongoose.model("Product", Product);
+module.exports = mongoose.model("Category", Category);
