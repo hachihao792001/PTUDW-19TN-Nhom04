1aca726338b256284d8652eafa45ecf0993c0ce6
TranPhuongDinh <dinhblade123@gmail.com>
2022-07-02 23:40:39 +0700

    fix header conflict and comment flow in payment controller

--- src/app/controllers/PaymentController.js
+++ src/app/controllers/PaymentController.js
@@ -1,6 +1,21 @@
 class PaymentController {
     //[GET] /about
     index(req, res, next) {
+        // get ds sản phẩm
+
+        // hard code giỏ hàng
+        // const cart = [
+        // 	{
+        // 		productId,
+        // 		quantity
+        // 	},...
+        // ]
+
+        // map productID với product
+
+        // => cart mapped = [{product, quantity}, ...]
+        // => đưa vô payment, render ra ở trang payment
+
         res.render("payment");
     }
 }
