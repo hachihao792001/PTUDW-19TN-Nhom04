8905cf9f5d69084cab02af715c1115fdca6232cc
ngoctinvo <ngoctinvo2001@gmail.com>
2022-07-03 00:26:17 +0700

    fix paymentController and render cart list to payment page

--- src/app/controllers/PaymentController.js
+++ src/app/controllers/PaymentController.js
@@ -1,22 +1,44 @@
+const Product = require("../models/Product");
+const { multipleMongooseToObject } = require("../../utils/mongoose");
+
 class PaymentController {
     //[GET] /about
-    index(req, res, next) {
+    async index(req, res, next) {
         // get ds sản phẩm
+        // let cart = await Cart.find({});
+        let products = await Product.find({});
+        products = multipleMongooseToObject(products);
 
         // hard code giỏ hàng
-        // const cart = [
-        // 	{
-        // 		productId,
-        // 		quantity
-        // 	},...
-        // ]
+        const cart = [{
+                productId: 1,
+                quantity: 3,
+            },
+            {
+                productId: 2,
+                quantity: 1,
+            },
+        ];
+        const mappedCart = cart.map((item) => {
+            const product = products.find(
+                (product) => (item.productId = product._id)
+            );
+            return {
+                id: item.productId,
+                name: product.name,
+                img: product.image,
+                price: product.price,
+                quantity: item.quantity,
+                total: product.price * item.quantity,
+            };
+        });
 
         // map productID với product
 
         // => cart mapped = [{product, quantity}, ...]
         // => đưa vô payment, render ra ở trang payment
 
-        res.render("payment");
+        res.render("payment", { mappedCart });
     }
 }
 
