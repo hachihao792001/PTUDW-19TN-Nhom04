8359b950b258ae7d9ffdbce91f4f56c96042f2ca
TranPhuongDinh <dinhblade123@gmail.com>
2022-07-03 20:39:37 +0700

    fix min height for main tag

--- src/app/controllers/AuthController.js
+++ src/app/controllers/AuthController.js
@@ -4,120 +4,122 @@ const Cart = require("../models/Cart");
 const jwt = require("jsonwebtoken");
 
 class SignInController {
-  async signUp(req, res, next) {
-    try {
-      // const formData = req.body;
-      // const email = formData.email;
-      const { name, phone, email, password, confirmPassword } = req.body;
-      let existingUser, _id;
+    async signUp(req, res, next) {
+        try {
+            // const formData = req.body;
+            // const email = formData.email;
+            const { name, phone, email, password, confirmPassword } = req.body;
+            let existingUser, _id;
 
-      existingUser = await User.findOne({ email });
+            existingUser = await User.findOne({ email });
 
-      if (existingUser) {
-        const error = new Error("User exists already", 422);
-        res.render("error", { message: "Tài khoản đã tồn tại" });
-      }
-      if (password != confirmPassword) {
-        const error = new Error("Confirm password does not match");
-        res.render("error", { message: "Xác nhận mật khẩu không đúng" });
-      }
+            if (existingUser) {
+                res.render("error", { message: "Tài khoản đã tồn tại" });
+            }
+            if (password != confirmPassword) {
+                res.render("error", {
+                    message: "Xác nhận mật khẩu không đúng",
+                });
+            }
 
-      // Get ID
-      const latestUser = await User.findOne({}, { upsert: true }).sort({
-        _id: "desc",
-      });
-      _id = (latestUser?._id || 0) + 1;
+            // Get ID
+            const latestUser = await User.findOne({}, { upsert: true }).sort({
+                _id: "desc",
+            });
+            _id = (latestUser?._id || 0) + 1;
 
-      let hashPassword;
-      // let password = formData.password;
+            let hashPassword;
+            // let password = formData.password;
 
-      const salt = bcrypt.genSaltSync(10);
-      hashPassword = bcrypt.hashSync(password, salt);
+            const salt = bcrypt.genSaltSync(10);
+            hashPassword = bcrypt.hashSync(password, salt);
 
-      // formData.password = hashPassword;
-      const createUser = new User({
-        email,
-        name,
-        phone,
-        password: hashPassword,
-        image:
-          "https://p16-sign.tiktokcdn-us.com/tos-useast5-avt-0068-tx/514b72b37695a35b1c0feab9e3ba63fd~c5_720x720.jpeg?x-expires=1656367200&x-signature=T%2BlCbPyCwj12piFwJJWFdG5yH3s%3D",
-        _id,
-        balance: 0,
-        address: "TP HCM",
-        accessDays: new Date().toDateString(),
-      });
+            // formData.password = hashPassword;
+            const createUser = new User({
+                email,
+                name,
+                phone,
+                password: hashPassword,
+                image: "https://p16-sign.tiktokcdn-us.com/tos-useast5-avt-0068-tx/514b72b37695a35b1c0feab9e3ba63fd~c5_720x720.jpeg?x-expires=1656367200&x-signature=T%2BlCbPyCwj12piFwJJWFdG5yH3s%3D",
+                _id,
+                balance: 0,
+                address: "TP HCM",
+                accessDays: new Date().toDateString(),
+            });
 
-      await createUser.save();
+            await createUser.save();
 
-      //create a new Cart with that user
-      const createCart = new Cart({
-        _id: _id,
-        userId: _id,
-        products: [],
-      });
+            //create a new Cart with that user
+            const createCart = new Cart({
+                _id: _id,
+                userId: _id,
+                products: [],
+            });
 
-      await createCart.save();
+            await createCart.save();
 
-      let token;
-      token = jwt.sign(
-        { adminId: createUser._id, email: createUser.email },
-        process.env.JWT_KEY,
-        { expiresIn: "1h" }
-      );
+            let token;
+            token = jwt.sign(
+                { adminId: createUser._id, email: createUser.email },
+                process.env.JWT_KEY,
+                { expiresIn: "1h" }
+            );
 
-      res.redirect("/");
+            res.redirect("/");
 
-      // res.status(201).json({
-      //   userId: createUser._id,
-      //   email: createUser.email,
-      //   password: createUser.password,
-      //   token: token,
-      // });
-    } catch {
-      res.render("error", { message: "Lỗi không xác định" });
+            // res.status(201).json({
+            //   userId: createUser._id,
+            //   email: createUser.email,
+            //   password: createUser.password,
+            //   token: token,
+            // });
+        } catch {
+            res.render("error", { message: "Lỗi không xác định" });
+        }
     }
-  }
 
-  async signIn(req, res, next) {
-    try {
-      const { email, password } = req.body;
+    async signIn(req, res, next) {
+        try {
+            const { email, password } = req.body;
 
-      let existingUser;
-      existingUser = await User.findOne({ email });
+            let existingUser;
+            existingUser = await User.findOne({ email });
 
-      if (!existingUser) {
-        const error = new Error("User not found");
-        res.render("error", { message: "Không tìm thấy tài khoản" });
-      }
+            if (!existingUser) {
+                const error = new Error("User not found");
+                res.render("error", { message: "Không tìm thấy tài khoản" });
+            }
 
-      let isValidPassword = false;
+            let isValidPassword = false;
 
-      isValidPassword = bcrypt.compareSync(password, existingUser.password);
+            isValidPassword = bcrypt.compareSync(
+                password,
+                existingUser.password
+            );
 
-      if (!isValidPassword) {
-        const error = new Error("Password is incorrect");
-        res.render("error", { message: "Sai mật khẩu" });
-      }
+            if (!isValidPassword) {
+                const error = new Error("Password is incorrect");
+                res.render("error", { message: "Sai mật khẩu" });
+            }
 
-      let token;
+            let token;
 
-      token = jwt.sign(
-        { userId: existingUser._id, email: existingUser.email },
-        process.env.JWT_KEY,
-        { expiresIn: "1h" }
-      );
-      res.cookie("token", token);
-      res.redirect("/");
-    } catch {
-      res.render("error", { message: "Lỗi không xác định" });
+            token = jwt.sign(
+                { userId: existingUser._id, email: existingUser.email },
+                process.env.JWT_KEY,
+                { expiresIn: "1h" }
+            );
+            res.cookie("token", token);
+            res.redirect("/");
+        } catch {
+            res.render("error", { message: "Lỗi không xác định" });
+        }
     }
-  }
 
-  logOut = (req, res) => {
-    res.cookie("token", "", { maxAge: 1 });
-    res.redirect("/");
-  };
+    logOut = (req, res) => {
+        res.cookie("token", "", { maxAge: 1 });
+        res.redirect("/");
+    };
 }
 
 module.exports = new SignInController();
