420079469ca6c3c0b2eee708486b5683cd804a55
TranPhuongDinh <dinhblade123@gmail.com>
2022-07-03 11:33:37 +0700

    add padding for input form

--- src/public/js/header.js
+++ src/public/js/header.js
@@ -1,9 +1,11 @@
-const SignInBtn = document.querySelector('.unloginNav');
-const Avatar = document.querySelector('.loginNav');
+const SignInBtn = document.querySelector(".unloginNav");
+const Avatar = document.querySelector(".loginNav");
+
+console.log(document.cookie);
 
 let token = document.cookie;
 if (!token) {
-  Avatar.style.display = 'none';
+    Avatar.style.display = "none";
 } else {
-  SignInBtn.style.display = 'none';
+    SignInBtn.style.display = "none";
 }
