6e069671e34b589bfc78baedf8eb14e434d7f0e2
ngoctinvo <ngoctinvo2001@gmail.com>
2022-07-03 13:31:13 +0700

    add rendering error page when sign in/sign up failed

--- src/app/controllers/AuthController.js
+++ src/app/controllers/AuthController.js
@@ -5,92 +5,78 @@ const jwt = require("jsonwebtoken");
 
 class SignInController {
   async signUp(req, res, next) {
-    // const formData = req.body;
-    // const email = formData.email;
-    const { name, phone, email, password, confirmPassword } = req.body;
-    let existingUser, _id;
-
     try {
+      // const formData = req.body;
+      // const email = formData.email;
+      const { name, phone, email, password, confirmPassword } = req.body;
+      let existingUser, _id;
+
       existingUser = await User.findOne({ email });
-    } catch (error) {
-      return next(error);
-    }
 
-    if (existingUser) {
-      const error = new Error("User exists already", 422);
-      return next(error);
-    }
-    if (password != confirmPassword) {
-      const error = new Error("Confirm password does not match");
-      return next(error);
-    }
+      if (existingUser) {
+        const error = new Error("User exists already", 422);
+        res.render("error", { message: "Tài khoản đã tồn tại" });
+      }
+      if (password != confirmPassword) {
+        const error = new Error("Confirm password does not match");
+        res.render("error", { message: "Xác nhận mật khẩu không đúng" });
+      }
 
-    // Get ID
-    const latestUser = await User.findOne({}, { upsert: true }).sort({
-      _id: "desc",
-    });
-    _id = (latestUser?._id || 0) + 1;
+      // Get ID
+      const latestUser = await User.findOne({}, { upsert: true }).sort({
+        _id: "desc",
+      });
+      _id = (latestUser?._id || 0) + 1;
 
-    let hashPassword;
-    // let password = formData.password;
-    try {
+      let hashPassword;
+      // let password = formData.password;
+
       const salt = bcrypt.genSaltSync(10);
       hashPassword = bcrypt.hashSync(password, salt);
-    } catch (error) {
-      return next(error);
-    }
-    // formData.password = hashPassword;
-    const createUser = new User({
-      email,
-      name,
-      phone,
-      password: hashPassword,
-      image:
-        "https://p16-sign.tiktokcdn-us.com/tos-useast5-avt-0068-tx/514b72b37695a35b1c0feab9e3ba63fd~c5_720x720.jpeg?x-expires=1656367200&x-signature=T%2BlCbPyCwj12piFwJJWFdG5yH3s%3D",
-      _id,
-      balance: 0,
-      address: "TP HCM",
-      accessDays: new Date().toDateString(),
-    });
 
-    try {
+      // formData.password = hashPassword;
+      const createUser = new User({
+        email,
+        name,
+        phone,
+        password: hashPassword,
+        image:
+          "https://p16-sign.tiktokcdn-us.com/tos-useast5-avt-0068-tx/514b72b37695a35b1c0feab9e3ba63fd~c5_720x720.jpeg?x-expires=1656367200&x-signature=T%2BlCbPyCwj12piFwJJWFdG5yH3s%3D",
+        _id,
+        balance: 0,
+        address: "TP HCM",
+        accessDays: new Date().toDateString(),
+      });
+
       await createUser.save();
-    } catch (error) {
-      next(error);
-    }
 
-    //create a new Cart with that user
-    const createCart = new Cart({
-      _id: _id,
-      userId: _id,
-      products: [],
-    });
+      //create a new Cart with that user
+      const createCart = new Cart({
+        _id: _id,
+        userId: _id,
+        products: [],
+      });
 
-    try {
       await createCart.save();
-    } catch (error) {
-      next(error);
-    }
 
-    let token;
-    try {
+      let token;
       token = jwt.sign(
         { adminId: createUser._id, email: createUser.email },
         process.env.JWT_KEY,
         { expiresIn: "1h" }
       );
-    } catch (error) {
-      next(error);
+
+      res.redirect("/");
+
+      // res.status(201).json({
+      //   userId: createUser._id,
+      //   email: createUser.email,
+      //   password: createUser.password,
+      //   token: token,
+      // });
+    } catch {
+      res.render("error", { message: "Lỗi không xác định" });
     }
-
-    res.redirect("/");
-
-    // res.status(201).json({
-    //   userId: createUser._id,
-    //   email: createUser.email,
-    //   password: createUser.password,
-    //   token: token,
-    // });
   }
 
   async signIn(req, res, next) {
@@ -98,11 +84,7 @@ class SignInController {
       const { email, password } = req.body;
 
       let existingUser;
-      try {
-        existingUser = await User.findOne({ email });
-      } catch (err) {
-        res.render("error", { message: "Lỗi không xác định" });
-      }
+      existingUser = await User.findOne({ email });
 
       if (!existingUser) {
         const error = new Error("User not found");
@@ -119,17 +101,14 @@ class SignInController {
       }
 
       let token;
-      try {
-        token = jwt.sign(
-          { userId: existingUser._id, email: existingUser.email },
-          process.env.JWT_KEY,
-          { expiresIn: "1h" }
-        );
-        res.cookie("token", token);
-        res.redirect("/");
-      } catch (err) {
-        res.render("error", { message: "Lỗi không xác định" });
-      }
+
+      token = jwt.sign(
+        { userId: existingUser._id, email: existingUser.email },
+        process.env.JWT_KEY,
+        { expiresIn: "1h" }
+      );
+      res.cookie("token", token);
+      res.redirect("/");
     } catch {
       res.render("error", { message: "Lỗi không xác định" });
     }
