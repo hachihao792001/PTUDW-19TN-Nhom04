459f3bf317388c95c39abba29a96f767a2776857
TranPhuongDinh <dinhblade123@gmail.com>
2022-07-04 17:45:29 +0700

    update price and quantity and show

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,122 +1,182 @@
-window.addEventListener('load', function () {
-	const API_URL = 'http://localhost:3000';
+window.addEventListener("load", function () {
+    const API_URL = "http://localhost:3000";
 
-  $('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus');
-  });
-
-  $(document).ready(function () {
-    AOS.init({ duration: 1000 });
-    $('.promotion-slider').slick({
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      dots: true,
-      arrows: true,
-      prevArrow: $('.slider-prev'),
-      nextArrow: $('.slider-next'),
-      autoplay: true,
-      autoplaySpeed: 1500,
-      responsive: [
-        {
-          breakpoint: 1200,
-          settings: {
-            slidesToShow: 2,
-          },
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 1,
-          },
-        },
-      ],
+    $("#myModal").on("shown.bs.modal", function () {
+        $("#myInput").trigger("focus");
     });
-    $('.images-slider').slick({
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      responsive: [
-        {
-          breakpoint: 1200,
-          settings: {
-            slidesToShow: 2,
-          },
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 1,
-          },
-        },
-      ],
+
+    $(document).ready(function () {
+        AOS.init({ duration: 1000 });
+        $(".promotion-slider").slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            dots: true,
+            arrows: true,
+            prevArrow: $(".slider-prev"),
+            nextArrow: $(".slider-next"),
+            autoplay: true,
+            autoplaySpeed: 1500,
+            responsive: [
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 2,
+                    },
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                    },
+                },
+            ],
+        });
+        $(".images-slider").slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            responsive: [
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 2,
+                    },
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                    },
+                },
+            ],
+        });
     });
-  });
 
-  const numberWithCommas = (x, unit = 'đ') => {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.') + unit;
-  };
+    const numberWithCommas = (x, unit = "đ") => {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") + unit;
+    };
 
-  const userId = document.querySelector('#userId').innerHTML;
-  const UpdateDBCart = async (product) => {
-    let formData = new FormData();
-    formData.append('userId', parseInt(userId));
-    formData.append('productId', parseInt(product._id));
-    formData.append('quantity', parseInt(product.quantity));
+    const updateLocalCart = (productId, quantity) => {
+        const cartJson = localStorage.getItem("cart") || "{}";
+        const localCart = JSON.parse(cartJson);
+        let { products } = localCart;
+        const newProducts = [...products].find(
+            (product) => product._id === productId
+        );
+        if (!newProducts) {
+            products.push({
+                _id: productId,
+                quantity,
+            });
+        } else {
+            for (let i = 0; i < products.length; i++) {
+                if (products[i]._id === productId) {
+                    if (quantity === 0 || products[i].quantity == 0) {
+                        products.splice(i, 1);
+                    } else {
+                        products[i].quantity = quantity;
+                    }
+                    break;
+                }
+            }
+        }
+        localStorage.setItem("cart", JSON.stringify({ products }));
+    };
 
-    try {
-      await fetch(`${API_URL}/cart`, {
-        method: 'POST',
-        body: formData,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    const userId = document.querySelector("#userId").innerHTML;
 
-  const DeleteDBCart = async (product) => {
-    let formData = new FormData();
-    formData.append('userId', parseInt(userId));
-    formData.append('productId', parseInt(product._id));
+    const calculateTotalPriceCart = () => {
+        const productsJson = localStorage.getItem("products") || "[]";
+        const productsConverted = JSON.parse(productsJson);
+        const { products } = productsConverted;
 
-    try {
-      await fetch(`${API_URL}/cart`, {
-        method: 'DELETE',
-        body: formData,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+        let productsMap = {};
 
-  const updatePopupCart = () => {
-    const productsJson = localStorage.getItem('products') || '[]';
-    const productsConverted = JSON.parse(productsJson);
-    const { products } = productsConverted;
+        for (let i = 0; i < products.length; i++) {
+            productsMap[products[i]._id] = products[i];
+        }
 
-    let productsMap = {};
+        const cartJson = localStorage.getItem("cart") || "{}";
+        const cart = JSON.parse(cartJson);
 
-    for (let i = 0; i < products.length; i++) {
-      productsMap[products[i]._id] = products[i];
-    }
+        let total = 0;
+        let totalQuantity = 0;
 
-    const cartJson = localStorage.getItem('cart') || '{}';
-    const cart = JSON.parse(cartJson);
+        if (cart?.products?.length > 0) {
+            cart?.products.forEach(function (product) {
+                const productItem = productsMap[product._id];
+                total += product.quantity * productItem.price;
+                totalQuantity += product.quantity;
+            });
+        }
+        $("#totalPriceCart").html(numberWithCommas(total));
+        $("#total-products-price").html(numberWithCommas(total));
+        $("#total-products-quantity").html(totalQuantity);
+    };
 
-    const popUpContent = $('#cartModal #myPopup');
-    popUpContent.html('');
+    calculateTotalPriceCart();
 
-    if (cart?.products?.length > 0) {
-      let total = 0;
+    const UpdateDBCart = async (product) => {
+        let formData = new FormData();
+        formData.append("userId", parseInt(userId));
+        formData.append("productId", parseInt(product._id));
+        formData.append("quantity", parseInt(product.quantity));
 
-      cart?.products.forEach(function (product) {
-        const productItem = productsMap[product._id];
-        total += product.quantity * productItem.price;
+        try {
+            await fetch(`${API_URL}/cart`, {
+                method: "POST",
+                body: formData,
+            });
+            updateLocalCart(product._id, product.quantity);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-        const productItemElements = $(
-          $.parseHTML(`
+    const DeleteDBCart = async (product) => {
+        let formData = new FormData();
+        formData.append("userId", parseInt(userId));
+        formData.append("productId", parseInt(product._id));
+
+        try {
+            await fetch(`${API_URL}/cart`, {
+                method: "DELETE",
+                body: formData,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const updatePopupCart = () => {
+        const productsJson = localStorage.getItem("products") || "[]";
+        const productsConverted = JSON.parse(productsJson);
+        const { products } = productsConverted;
+
+        let productsMap = {};
+
+        for (let i = 0; i < products.length; i++) {
+            productsMap[products[i]._id] = products[i];
+        }
+
+        const cartJson = localStorage.getItem("cart") || "{}";
+        const cart = JSON.parse(cartJson);
+
+        const popUpContent = $("#cartModal #myPopup");
+        popUpContent.html("");
+
+        if (cart?.products?.length > 0) {
+            let total = 0;
+
+            cart?.products.forEach(function (product) {
+                const productItem = productsMap[product._id];
+                total += product.quantity * productItem.price;
+
+                const productItemElements = $(
+                    $.parseHTML(`
                         <div class='item'>
                             <div class='img'>
                                 <img src="${productItem.image}" 
@@ -126,7 +186,7 @@ window.addEventListener('load', function () {
                                 <h3>${productItem.name}</h3>
                                 <p>${productItem.description}</p>
                                 <span>Giá: ${numberWithCommas(
-                                  productItem.price
+                                    productItem.price
                                 )}</span>
                             </div>
                             <div class='select'>
@@ -144,140 +204,144 @@ window.addEventListener('load', function () {
                             </div>
                         </div>
                     `)
-        );
+                );
 
-        const minusButton = productItemElements.find('.minus-cart-button');
-        const plusButton = productItemElements.find('.plus-cart-button');
-        const quantityInput = productItemElements.find('.quantity-cart-input');
+                const minusButton =
+                    productItemElements.find(".minus-cart-button");
+                const plusButton =
+                    productItemElements.find(".plus-cart-button");
+                const quantityInput = productItemElements.find(
+                    ".quantity-cart-input"
+                );
 
-        minusButton.on('click', function () {
-          const quantity = parseInt(quantityInput.val());
-          if (quantity > 1) {
-            quantityInput.val(quantity - 1);
-            UpdateDBCart({ _id: productItem._id, quantity: quantity - 1 });
-          }
-        });
-        plusButton.on('click', function () {
-          const quantity = parseInt(quantityInput.val());
-          quantityInput.val(quantity + 1);
-          UpdateDBCart({ _id: productItem._id, quantity: quantity + 1 });
-        });
+                minusButton.on("click", async function () {
+                    const quantity = parseInt(quantityInput.val());
+                    if (quantity > 1) {
+                        quantityInput.val(quantity - 1);
+                        await UpdateDBCart({
+                            _id: productItem._id,
+                            quantity: quantity - 1,
+                        });
+                        calculateTotalPriceCart();
+                    }
+                });
+                plusButton.on("click", async function () {
+                    const quantity = parseInt(quantityInput.val());
+                    quantityInput.val(quantity + 1);
+                    await UpdateDBCart({
+                        _id: productItem._id,
+                        quantity: quantity + 1,
+                    });
+                    calculateTotalPriceCart();
+                });
 
-        popUpContent.append(productItemElements);
-      });
+                quantityInput.on("change", async function () {
+                    const quantity = parseInt(quantityInput.val());
+                    if (quantity < 1) {
+                        quantityInput.val(1);
+                        await UpdateDBCart({
+                            _id: productItem._id,
+                            quantity: 1,
+                        });
+                        return;
+                    }
+                    await UpdateDBCart({ _id: productItem._id, quantity });
+                    calculateTotalPriceCart();
+                });
 
-      popUpContent.append(`
-					<div class='total'>
-						<span id='TotalAmount'>Tổng tiền: ${numberWithCommas(total)}</span>
-					</div>
+                popUpContent.append(productItemElements);
+            });
+
+            popUpContent.append(`
+			<div class='total'>
+			<span>Tổng tiền: <span id="totalPriceCart">${numberWithCommas(
+                total
+            )}</span></span>
+		</div>
 				`);
-    } else {
-      popUpContent.append(`
-					<div class='total'>
-						<span id='TotalAmount'>Tổng tiền: 0</span>
-					</div>
+        } else {
+            popUpContent.append(`
+			<div class='total'>
+			<span>Tổng tiền: <span id="totalPriceCart">${numberWithCommas(0)}</span></span>
+		</div>
 				`);
-    }
-  };
-
-  updatePopupCart();
-
-  const btnAddProducts = document.querySelectorAll('.btn-add');
-
-  btnAddProducts.forEach(function (btn) {
-    btn.addEventListener('click', function () {
-      const localCart = localStorage.getItem('cart') || '{}';
-      const cart = JSON.parse(localCart) || {};
-      const productId = this.getAttribute('data-product-id');
-
-      let hasProduct = false;
-
-      if (cart['products']) {
-        cart['products'].forEach(function (product) {
-          if (product._id === parseInt(productId)) {
-            hasProduct = true;
-          }
-        });
-      } else {
-        cart['products'] = [];
-        cart['products'].push({
-          _id: parseInt(productId),
-          quantity: 1,
-        });
-      }
-
-      if (hasProduct) {
-        this.innerText = 'Xóa khỏi giỏ hàng';
-      }
-
-      localStorage.setItem('cart', JSON.stringify(cart));
-    });
-  });
-
-  btnAddProducts.forEach(function (btn) {
-    const localCart = localStorage.getItem('cart') || '{}';
-    const cart = JSON.parse(localCart) || {};
-    const productId = btn.getAttribute('data-product-id');
-    let hasProduct = false;
-
-    if (cart['products']) {
-      cart['products'].forEach(function (product) {
-        if (product._id === parseInt(productId)) {
-          hasProduct = true;
-          btn.innerText = 'Xóa khỏi giỏ hàng';
         }
-      });
-    }
+    };
 
     updatePopupCart();
 
-    btn.addEventListener('click', function () {
-      const localCart = localStorage.getItem('cart') || '{}';
-      const cart = JSON.parse(localCart) || {};
-      const productId = btn.getAttribute('data-product-id');
-      let hasProduct = false;
+    const btnAddProducts = document.querySelectorAll(".btn-add");
 
-      if (cart['products']) {
-        cart['products'].forEach(function (product) {
-          if (product._id === parseInt(productId)) {
-            hasProduct = true;
-          }
+    btnAddProducts.forEach(function (btn) {
+        const localCart = localStorage.getItem("cart") || "{}";
+        const cart = JSON.parse(localCart) || {};
+        const productId = btn.getAttribute("data-product-id");
+        let hasProduct = false;
+
+        if (cart["products"]) {
+            cart["products"].forEach(function (product) {
+                if (product._id === parseInt(productId)) {
+                    hasProduct = true;
+                    btn.innerText = "Xóa";
+										btn.classList.add("btn-delete");
+                }
+            });
+        }
+
+        updatePopupCart();
+
+        btn.addEventListener("click", async function () {
+            const localCart = localStorage.getItem("cart") || "{}";
+            const cart = JSON.parse(localCart) || {};
+            const productId = btn.getAttribute("data-product-id");
+            let hasProduct = false;
+
+            if (cart["products"]) {
+                cart["products"].forEach(function (product) {
+                    if (product._id === parseInt(productId)) {
+                        hasProduct = true;
+                    }
+                });
+            }
+
+            console.log("Cart", cart);
+
+            if (hasProduct) {
+                cart["products"].forEach(function (product, index) {
+                    if (product._id == productId) {
+                        cart["products"].splice(index, 1);
+                    }
+                });
+								localStorage.setItem("cart", JSON.stringify(cart));
+                await DeleteDBCart({ _id: productId });
+                calculateTotalPriceCart();
+                this.innerText = "Thêm";
+								btn.classList.remove("btn-delete");
+            } else {
+                let newProduct = {
+                    _id: parseInt(productId),
+                    quantity: 1,
+                };
+                cart["products"].push(newProduct);
+								localStorage.setItem("cart", JSON.stringify(cart));
+                await UpdateDBCart(newProduct);
+                calculateTotalPriceCart();
+                this.innerText = "Xóa";
+								btn.classList.add("btn-delete");
+            }
+
+            updatePopupCart();
         });
-      }
-
-      console.log('Cart', cart);
-
-      if (hasProduct) {
-        cart['products'].forEach(function (product, index) {
-          if (product._id == productId) {
-            cart['products'].splice(index, 1);
-          }
-        });
-        DeleteDBCart({ _id: productId });
-        this.innerText = 'Thêm';
-      } else {
-        let newProduct = {
-          _id: parseInt(productId),
-          quantity: 1,
-        };
-        cart['products'].push(newProduct);
-        UpdateDBCart(newProduct);
-        this.innerText = 'Xóa khỏi giỏ hàng';
-      }
-
-      localStorage.setItem('cart', JSON.stringify(cart));
-      updatePopupCart();
     });
-  });
 
-  $('#cartModal').on('shown.bs.modal', function () {
-    updatePopupCart();
-  });
+    $("#cartModal").on("shown.bs.modal", function () {
+        updatePopupCart();
+    });
 });
 
 const openCart = function () {
-  console.log('chay vo open cart roi');
-  document.getElementById('cart').click();
+    console.log("chay vo open cart roi");
+    document.getElementById("cart").click();
 };
 
-document.querySelector('#btnOrderNow').addEventListener('click', openCart);
+document.querySelector("#btnOrderNow").addEventListener("click", openCart);
